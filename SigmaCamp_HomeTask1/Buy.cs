8e8ca3ad635950a54c4fe1578a872f063d8b866d
Ferum2704 <protsysyn.bohdan@gmail.com>
2022-05-19 17:39:59 +0300

    Added field TotalPrice

--- SigmaCamp_HomeTask1/Buy.cs
+++ SigmaCamp_HomeTask1/Buy.cs
@@ -4,14 +4,18 @@ namespace SigmaCamp_HomeTask1
 {
     internal class Buy
     {
+        public static List<Buy> boughtProducts { get; }
         public Product ProductToBuy { get; private set; }
         private int _productQuantity;
+        public decimal TotalPrice { get; private set; }
         public double ProductTotalWeight { get; private set; }
         public Buy(Product product, int quantity)
         {
             ProductToBuy = product;
             _productQuantity = quantity;
+            TotalPrice = ProductToBuy.Price * quantity;
             ProductTotalWeight = ProductToBuy.Weight * quantity;
+            boughtProducts.Add(this);
         }
         public int ProductQuantity
         {
