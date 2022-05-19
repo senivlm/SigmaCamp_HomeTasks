c9714ba1133d77b0da61f3a1808180e5c4933f0c
Ferum2704 <protsysyn.bohdan@gmail.com>
2022-05-19 20:09:27 +0300

    Added properties Category and Kind to Meat class

---
+++ SigmaCamp_HomeTask1/Meat.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SigmaCamp_HomeTask1
+{
+    internal class Meat:Product
+    {
+        private string _category;
+        private string _kind;
+        public Meat(decimal price, double weight, string category, string kind):base("М'ясо", price, weight)
+        {
+            Category = category;
+            Kind = kind;
+        }
+        public string Category
+        {
+            get => _category;
+            set
+            {
+                switch (value)
+                {
+                    case "Extra class 1":
+                        _category = value;
+                        break;
+                    case "Extra class 2":
+                        _category = value;
+                        break;
+                    default:
+                        throw new ArgumentException("Category can be only Extra class 1 or 2");
+                }
+            }
+        }
+        public string Kind
+        {
+            get => _kind;
+            set
+            {
+                switch (value)
+                {
+                    case "mutton":
+                        _category = value;
+                        break;
+                    case "veal":
+                        _category = value;
+                        break;
+                    case "pork":
+                        _category = value;
+                        break;
+                    case "chicken":
+                        _category = value;
+                        break;
+                    default:
+                        throw new ArgumentException("Category can be only Extra class 1 or 2");
+                }
+            }
+        }
+    }
+}
