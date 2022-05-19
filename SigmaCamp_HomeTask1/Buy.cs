fd917326948d84b10ebd5c2f9a824d6b54db19e2
Ferum2704 <protsysyn.bohdan@gmail.com>
2022-05-19 17:21:50 +0300

    Finished Buy class

--- SigmaCamp_HomeTask1/Buy.cs
+++ SigmaCamp_HomeTask1/Buy.cs
@@ -4,6 +4,29 @@ namespace SigmaCamp_HomeTask1
 {
     internal class Buy
     {
-
+        public Product ProductToBuy { get; private set; }
+        private int _productQuantity;
+        public double ProductTotalWeight { get; private set; }
+        public Buy(Product product, int quantity)
+        {
+            ProductToBuy = product;
+            _productQuantity = quantity;
+            ProductTotalWeight = ProductToBuy.Weight * quantity;
+        }
+        public int ProductQuantity
+        {
+            get { return _productQuantity; }
+            set
+            {
+                if (value<=0)
+                {
+                    throw new ArgumentOutOfRangeException("There is an incorrect value for product`s weight");
+                }
+                else
+                {
+                    _productQuantity=value;
+                }
+            }
+        }
     }
 }
