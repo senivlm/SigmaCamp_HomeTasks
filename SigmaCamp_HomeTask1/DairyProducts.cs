41bad07637d562dd9a7b96b88c76fcaa3333fefa
Ferum2704 <protsysyn.bohdan@gmail.com>
2022-05-19 20:35:48 +0300

    Created Dairy Products class and added shelfLife property

---
+++ SigmaCamp_HomeTask1/DairyProducts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SigmaCamp_HomeTask1
+{
+    internal class DairyProducts:Product
+    {
+        private int _shelfLife;
+        public DairyProducts(string name, decimal price, double weight, int shelfLife):base(name, price, weight)
+        {
+            ShelfLife = shelfLife;
+        }
+        public int ShelfLife
+        {
+            get => _shelfLife;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("There is an incorrect value for product`s shelf life");
+                }
+                else
+                {
+                    _shelfLife = value;
+                }
+            }
+        }
+
+    }
+}
