cdd08afcba9bdffc24bad0674066eef1c944b225
Ferum2704 <protsysyn.bohdan@gmail.com>
2022-05-20 16:24:24 +0300

    Changed Category and Kind properties

--- SigmaCamp_HomeTask1/Meat.cs
+++ SigmaCamp_HomeTask1/Meat.cs
@@ -2,68 +2,60 @@
 
 namespace SigmaCamp_HomeTask1
 {
+    public enum MeatCategory
+    {
+        ExtraClass1,
+        ExtraClass2
+    }
+    public enum MeatKind
+    {
+        mutton,
+        veal,
+        pork,
+        chicken
+    }
     internal class Meat:Product
     {
         const double rateCategory1 = 0.1;
         const double rateCategory2 = 0.15;
-        private string _category;
-        private string _kind;
-        public Meat(decimal price, double weight, string category, string kind):base("М'ясо", price, weight)
+        private MeatCategory _category;
+        private MeatKind _kind;
+        public Meat(decimal price, double weight, string category, string kind):base("Meat", price, weight)
         {
             Category = category;
             Kind = kind;
         }
         public string Category
         {
-            get => _category;
+            get => _category.ToString();
             set
             {
-                switch (value)
+                if (!Enum.TryParse(value, true, out _category))
                 {
-                    case "Extra class 1":
-                        _category = value;
-                        break;
-                    case "Extra class 2":
-                        _category = value;
-                        break;
-                    default:
-                        throw new ArgumentException("Category can be only Extra class 1 or 2");
+                    throw new ArgumentException("There is no such kind or category of meat");
                 }
             }
         }
         public string Kind
         {
-            get => _kind;
+            get => _kind.ToString();
             set
             {
-                switch (value)
+                if (!Enum.TryParse(value, true, out _kind))
                 {
-                    case "mutton":
-                        _category = value;
-                        break;
-                    case "veal":
-                        _category = value;
-                        break;
-                    case "pork":
-                        _category = value;
-                        break;
-                    case "chicken":
-                        _category = value;
-                        break;
-                    default:
-                        throw new ArgumentException("Category can be only Extra class 1 or 2");
+                    throw new ArgumentException("There is no such kind or category of meat");
                 }
             }
         }
-        public override void ChangePrice(int percentToIncrease)
+        public override void ChangePrice(int percentToChange)
         {
-            base.ChangePrice(percentToIncrease);
-            switch (this.Category)
+            base.ChangePrice(percentToChange);
+            switch (_category)
             {
-                case "Extra class 1":
+                case MeatCategory.ExtraClass1:
                     Price = Price * (decimal)(rateCategory1+1);
                     break;
-                case "Extra class 2":
+                case MeatCategory.ExtraClass2:
                     Price = Price * (decimal)(rateCategory2 + 1);
                     break;
                 default:
@@ -72,7 +64,7 @@ namespace SigmaCamp_HomeTask1
         }
         public override string ToString()
         {
-            return $"{Name} {Kind}, {Category}, {Price} UAH for {Weight} kg";
+            return $"{Name} {_kind}, {_category}";
         }
     }
 }
