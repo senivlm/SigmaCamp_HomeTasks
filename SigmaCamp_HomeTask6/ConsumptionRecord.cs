983daa885a5ea78efdb940239f735cf349632f57
Ferum2704 <protsysyn.bohdan@gmail.com>
2022-06-14 13:21:04 +0300

    Added home task 6

---
+++ SigmaCamp_HomeTask6/ConsumptionRecord.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Globalization;
+
+namespace SigmaCamp_HomeTask6
+{
+    internal class ConsumptionRecord
+    {
+        DateTime _date = new DateTime();
+        int _startCounterValue;
+        int _endCounterValue;
+        int _quarter;
+        public ConsumptionRecord(string date, int startCounterValue, int endCounterValue, int quarter)
+        {
+
+            StartCounterValue = startCounterValue;
+            EndCounterValue = endCounterValue;
+            Quarter = quarter;
+            InitDate(date);
+        }
+        private void InitDate(string date)
+        {
+            date.Replace('.', '/');
+            date.LastIndexOf('/');
+            date.Insert(date.LastIndexOf('/') + 1, "20");
+            DateTime someDate;
+            if (DateTime.TryParse(date, out someDate))
+            {
+                _date = new DateTime(someDate.Year, someDate.Month, someDate.Day);
+            }
+        }
+        public DateTime Date
+        {
+            get { return _date; }
+        }
+        public int StartCounterValue
+        {
+            get { return _startCounterValue; }
+            init
+            {
+                if (value > 0)
+                {
+                    _startCounterValue = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+        public int EndCounterValue
+        {
+            get { return _endCounterValue; }
+            init
+            {
+                if (value > 0)
+                {
+                    _endCounterValue = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+        public int Quarter
+        {
+            get { return _quarter; }
+            init
+            {
+                if (value > 0 && value < 4)
+                {
+                    _quarter = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Incorrect value for quartel");
+                }
+            }
+        }
+        public string GetMonthName()
+        {
+            string monthName = string.Empty;
+            if (_date.Day >= 1 && _date.Day <= 3)
+            {
+                monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(_date.Month - 1);
+            }
+            else
+            {
+                monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(_date.Month);
+            }
+            monthName = char.ToUpper(monthName[0]) + monthName.Substring(1);
+            return monthName;
+        }
+        public override string ToString()
+        {
+            return $"Used electricity on {GetMonthName()}: {EndCounterValue - StartCounterValue}";
+        }
+    }
+}
---
+++ SigmaCamp_HomeTask6/Sentence.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigmaCamp_HomeTask6
+{
+    internal class Sentence
+    {
+        string[] _words;
+        public Sentence(string[] words)
+        {
+            if (words != null)
+            {
+                _words = words;
+            }
+            else
+            {
+                throw new ArgumentNullException("No words to create sentence");
+            }
+        }
+        public string GetLongestWords()
+        {
+            string maxWord = _words[0];
+            string maxWords = _words[0];
+            for (int i = 1; i < _words.Length; i++)
+            {
+                if (_words[i].Length > maxWord.Length)
+                {
+                    maxWord = maxWords = _words[i];
+                }
+                else if (_words[i].Length == maxWord.Length)
+                {
+                    maxWords+= ", "+ _words[i];
+                }
+            }
+            return maxWords;
+        }
+        public string GetShortestWords()
+        {
+            string minWord = _words[0];
+            string minWords = _words[0];
+            for (int i = 1; i < _words.Length; i++)
+            {
+                if (_words[i].Length < minWord.Length)
+                {
+                    minWord = minWords = _words[i];
+                }
+                else if (_words[i].Length == minWord.Length)
+                {
+                    minWords += ", " + _words[i];
+                }
+            }
+            return minWords;
+        }
+        public override string ToString()
+        {
+            string sentence = string.Empty;
+            for (int i = 0; i < _words.Length; i++)
+            {
+                sentence += $" {_words[i]}";
+            }
+            return sentence;
+        }
+    }
+}
