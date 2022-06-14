d731693829f11f4f019a051a22331334e5d68e59
Ferum2704 <protsysyn.bohdan@gmail.com>
2022-06-14 13:05:48 +0300

    Added home task 3

---
+++ SigmaCamp_HomeTask3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SigmaCamp_HomeTask3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector myVector = new Vector(10);
+            myVector.InitRandom(1, 4);
+            try
+            {
+                //for (int i = 0; i < myVector.GetLength()/2; i++)
+                //{
+                //    myVector[i] = i;
+                //    myVector[myVector.GetLength() - 1 - i] = i;
+                //}
+                Console.WriteLine(myVector);
+                //foreach (var item in myVector.CalculateFrequency())
+                //{
+                //    Console.WriteLine(item);
+                //}
+                //Pair pair1 = new Pair(2,5);
+                //Pair pair2 = new Pair(2,5);
+                //Console.WriteLine(pair1.Equals(pair2));
+                //if (myVector.CheckForPalindrome())
+                //{
+                //    Console.WriteLine("This array is palindrome");
+                //}
+                //myVector.Reverse();
+                //Console.WriteLine($"Result of my reverse method: {myVector}");
+                //myVector.BuiltInReverse();
+                //Console.WriteLine($"Result of built-in reverse method: {myVector}");
+                Console.WriteLine(myVector.GetLongestSequence());
+
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ SigmaCamp_HomeTask3/Vector.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace SigmaCamp_HomeTask3
+{
+    internal class Vector
+    {
+        int[] arr;
+        public Vector() : this(10) { }
+        public Vector(int size)
+        {
+            arr = new int[size];
+        }
+        public int this[int index]
+        {
+            get
+            {
+                if (index<0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return arr[index];
+            }
+            set
+            {
+                arr[index] = value;
+            }
+        }
+        public int GetLength()
+        {
+            return arr.Length;
+        }
+        public void InitRandom(int a, int b)
+        {
+            Random randomInt = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = randomInt.Next(a, b);
+            }
+        }
+        public void InitShuffle()
+        {
+            Random randomInt = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = 0;
+                bool isInit = false;
+                do
+                {
+                    int r = randomInt.Next(1, arr.Length + 1);
+                    if (Array.IndexOf(arr, r) < 0)
+                    {
+                        arr[i] = r;
+                        isInit = true;
+                    }
+                } while (!isInit);
+            }
+
+        }
+        public Pair[] CalculateFrequency()
+        {
+            Pair[] pairs = new Pair[arr.Length];
+            for (int i = 0; i < pairs.Length; i++)
+            {
+                pairs[i] = new Pair();
+            }
+            int countDifference = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isElement = false;
+                for (int j = 0; j < countDifference; j++)
+                {
+                    if (arr[i]==pairs[j].Number)
+                    {
+                        pairs[j].Frequency++;
+                        isElement = true;
+                        break;
+                    }
+                }
+                if (!isElement)
+                {
+                    pairs[countDifference].Frequency++;
+                    pairs[countDifference].Number = arr[i];
+                    countDifference++;
+                }
+            }
+            Array.Resize<Pair>(ref pairs, countDifference);
+            return pairs;
+        }
+        public bool CheckForPalindrome()
+        {
+            bool isPalindrome = false;
+            for (int i = 0, j = arr.Length-1; i < arr.Length/2; i++, j--)
+            {
+                if (arr[i]==arr[j])
+                {
+                    isPalindrome = true;
+                }
+                else
+                {
+                    isPalindrome = false;
+                    break;
+                }
+
+            }
+            return isPalindrome;           
+        }
+        public void Reverse()
+        {
+            for (int i = 0; i < arr.Length/2; i++)
+            {
+                int temp = arr[i];
+                arr[i] = arr[arr.Length-1-i];
+                arr[arr.Length-1-i] = temp;
+            }
+        }
+        public void BuiltInReverse()
+        {
+            Array.Reverse(arr);
+        }
+        public string GetLongestSequence()
+        {
+            string currentSequence = string.Empty;
+            string maxSequence = string.Empty;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                currentSequence = arr[i].ToString();
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        currentSequence += " " + arr[j];
+                        if (currentSequence.Length >= maxSequence.Length)
+                        {
+                            maxSequence = currentSequence;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return maxSequence;
+        }
+        public override string ToString()
+        {
+            string stringArr = "";
+            foreach (var item in arr)
+            {
+                stringArr += item + " ";
+            }
+            return stringArr;
+        }
+    }
+}
