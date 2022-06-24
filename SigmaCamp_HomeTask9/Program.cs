654a81c64826cbc198209618830eea65ce9e176d
Ferum2704 <protsysyn.bohdan@gmail.com>
2022-06-24 11:23:23 +0300

    work on lesson 14-06

---
+++ SigmaCamp_HomeTask9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesson14_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,double> productPrice= new Dictionary<string,double>();
+            productPrice.Add("tomato", 10);
+            productPrice.Add("Onion", 30);
+            productPrice.Add("Meat", 50);
+
+            List<string> products= new List<string>();
+
+        }
+    }
+}
