377e608b333b244e0cee4c5cbc0c87019fda0a07
Ferum2704 <protsysyn.bohdan@gmail.com>
2022-07-03 17:00:58 +0300

    Some changes to homeTask12_3

--- SigmaCamp_HomeTask12_3/Program.cs
+++ SigmaCamp_HomeTask12_3/Program.cs
@@ -10,10 +10,11 @@ namespace SigmaCamp_HomeTask12_3
             string expression = "20/2+(130.23-1)*cos(84-48)+sin(2.44+1.01*0.77)";
             PolishInvertor mather = new(expression);
             //Console.WriteLine(mather.GetPostfixExpr(expression));
-            Console.WriteLine("Вхідний вираз: " + mather.InputExpr);
-            Console.WriteLine("Постфіксна форма виразу: " + mather.PostfixExpr);
+            Console.WriteLine("Вхідний вираз: " + mather.InputExpr+"\n");
+            Console.WriteLine("Постфіксна форма виразу: " + mather.PostfixExpr+"\n");
             Console.WriteLine("Дії:");
             Console.WriteLine("Значення виразу: " + PolishCalculator.Calc(mather));
+            Console.WriteLine();
             Console.WriteLine($"Перевірка: {20/2+(130.23-1)*Math.Cos(84-48)+Math.Sin(2.44+1.01*0.77)}");
         }
     }
